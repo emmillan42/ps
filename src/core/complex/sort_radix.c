e16c00b70194c0d710fa8a01ec00feb18c8604dd
Durian Sosa <durisosa@c1r12s1.42madrid.com>
2026-07-20 15:44:14 +0200

    radix added

--- src/core/complex/sort_radix.c
+++ src/core/complex/sort_radix.c
@@ -5,17 +5,60 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: durisosa <durisosa@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/15 15:40:14 by durisosa          #+#    #+#             */
-/*   Updated: 2026/07/17 14:55:26 by durisosa         ###   ########.fr       */
+/*   Created: 2026/07/18 09:46:24 by emmmilla          #+#    #+#             */
+/*   Updated: 2026/07/20 15:41:41 by durisosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/**
- * RADIX
- */
+int	get_max_bits(t_stack *stack)
+{
+	int	max_index;
+	int	bits;
+
+	max_index = stack->size - 1;
+	bits = 1;
+	while (max_index > 1)
+	{
+		max_index >>= 1;
+		bits++;
+	}
+	return (bits);
+}
+
+static void	push_back_to_a(t_stack *a, t_stack *b)
+{
+	while (b->size > 0)
+		ft_pa(a, b);
+}
+
+static void	process_bit(t_stack *a, t_stack *b, int bit)
+{
+	int	size;
+
+	size = a->size;
+	while (size > 0)
+	{
+		if (((a->head->index >> bit) & 1) == 0)
+			ft_pb(a, b);
+		else
+			ft_ra(a);
+		size--;
+	}
+}
+
 void	sort_radix(t_stack *a, t_stack *b)
 {
+	int	bit;
+	int	max_bits;
 
+	max_bits = get_max_bits(a);
+	bit = 0;
+	while (bit < max_bits)
+	{
+		process_bit(a, b, bit);
+		push_back_to_a(a, b);
+		bit++;
+	}
 }
